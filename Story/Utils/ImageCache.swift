e8507f2dcdf5824e0724a79bb8ca554cffb24613
Emil Marashliev <emarashliev@vmware.com>
2022-09-26 15:17:14 +0300

    Optimize image cache

--- Story/Utils/ImageCache.swift
+++ Story/Utils/ImageCache.swift
@@ -4,23 +4,33 @@ import UIKit
 @globalActor actor ImageCache {
     static let shared  = ImageCache()
     
-    private let cachedImages: NSCache<NSURL, UIImage>
- 
+    private var cachedImages: NSCache<NSString, UIImage>
+
     init() {
-        cachedImages = NSCache<NSURL, UIImage>()
+        cachedImages = NSCache<NSString, UIImage>()
         cachedImages.totalCostLimit = 50_000_000
     }
     
-    func load(url: String?, scale: CGFloat) async throws -> UIImage? {
-        guard let url = url, let nsurl = NSURL(string: url) else { return  nil}
+    nonisolated func load(url: String?, scale: CGFloat) async throws -> UIImage? {
+        guard let url = url, let url = URL(string: url) else { return  nil}
         
-        if let cachedImage = cachedImages.object(forKey: nsurl) {
+        if let cachedImage = await getImage(with: url) {
             return cachedImage
         }
         
-        let (data, _) = try await URLSession.shared.data(from: nsurl as URL)
+        let (data, _) = try await URLSession.shared.data(from: url)
         guard let image = UIImage(data: data, scale: scale) else { return nil }
-        self.cachedImages.setObject(image, forKey: nsurl, cost: image.jpegData(compressionQuality: 1)?.count ?? 0)
+        await add(image: image, with: url)
         return image
+    }
+    
+    private func add(image: UIImage, with url: URL) {
+        let key = url.absoluteString as NSString
+        cachedImages.setObject(image, forKey: key, cost: image.jpegData(compressionQuality: 1)?.count ?? 0)
+    }
+    
+    private func getImage(with url: URL) -> UIImage? {
+        let key = url.absoluteString as NSString
+        return cachedImages.object(forKey: key)
     }
 }
